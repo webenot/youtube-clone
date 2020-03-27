30334c2dd10f6550e59ca98bc981c6fb73ff36e8
Halyna Urbanska <webenot@mail.ua>
2020-03-27 18:29:41 +0200

    Render fake video list

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
+import { videos } from '../db';
+
+export const home = (req, res) => {
+  res.render('home', { pageTitle: 'Home', videos });
+};
 
 export const search = (req, res) => {
   const { query: { term: searchingBy } } = req;
@@ -7,5 +11,13 @@ export const search = (req, res) => {
 
 export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
-export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
-export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
+
+export const editVideo = (req, res) => {
+  const { params: { id: videoId } } = req;
+  console.log(req.params);
+  res.render('editVideo', { pageTitle: 'Edit Video', videoId });
+};
+
+export const deleteVideo = (req, res) => {
+  res.render('deleteVideo', { pageTitle: 'Delete Video' });
+};
---
+++ db.js
@@ -0,0 +1,26 @@
+export const videos = [
+  {
+    id: 234,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      name: 'Nicolas',
+      email: 'nico@las.com',
+      id: 1212
+    }
+  },
+  {
+    id: 2345,
+    title: 'Video awesome 2',
+    description: 'This is something I love',
+    views: 3,
+    videoFile: 'https://archive.org/details/BigBuckBunny_124',
+    creator: {
+      name: 'Nicolas',
+      email: 'nico@las.com',
+      id: 1212
+    }
+  }
+];
