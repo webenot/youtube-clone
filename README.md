61933de139369654c187fa3323501a0d886bd625
Halyna Urbanska <webenot@mail.ua>
2020-03-27 18:04:34 +0200

    Edit Profile page

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Cloning Youtube with VanillaJS and NodeJS
 ## Pages:
 
 - [ ] Home
-- [ ] Join
-- [ ] Login
+- [x] Join
+- [x] Login
 - [x] Search
 - [ ] User Detail
-- [ ] Edit Profile
+- [x] Edit Profile
 - [ ] Change Password
 - [ ] Upload
 - [ ] Video Detail
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,8 +6,8 @@ import routes from '../routes';
 const userRouter = express.Router();
 
 userRouter.get(routes.home, home);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
